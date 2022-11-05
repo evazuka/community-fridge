b8f525c715f940343da9585132165d3fc17db121
Dmitrijs Minajevs <dmitrijs.minajevs@outlook.com>
2022-11-05 10:46:15 +0200

    Start share page styling

--- pages/share.tsx
+++ pages/share.tsx
@@ -3,23 +3,19 @@ import { Auth, ThemeSupa } from "@supabase/auth-ui-react"
 import { UploadImage } from "../components/uploadImage"
 
 const Share = () => {
-    const session = useSession()
-    const supabase = useSupabaseClient()
+  const session = useSession()
+  const supabase = useSupabaseClient()
 
-    if (!session) return <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} theme="dark" />
+  if (!session) return <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} theme="dark" />
 
-    return (
-        <div className="container" style={{ padding: '50px 0 100px 0' }}>
-          {!session ? (
-            <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} theme="dark" />
-          ) : (
-            <>
-              <div>Hello, {session.user.email}</div>
-              <div><UploadImage url={null} /></div>
-            </>
-          )}
-        </div>
-      )
+  return (
+    <div className="container" style={{ padding: '50px 0 100px 0' }}>
+      <>
+        <div>Hello, {session.user.email}</div>
+        <div><UploadImage url={null} /></div>
+      </>
+    </div>
+  )
 }
 
 export default Share
