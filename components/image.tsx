73bab4c299f76e43994d4a197f4b235c6bb6fc5e
Dmitrijs Minajevs <dmitrijs.minajevs@outlook.com>
2022-11-05 09:36:14 +0200

    Separate components

---
+++ components/image.tsx
@@ -0,0 +1,31 @@
+import { useSupabaseClient } from "@supabase/auth-helpers-react"
+import { useEffect, useState } from "react"
+
+type Props = {
+  url: string | null
+}
+
+export const Image = ({ url }: Props) => {
+  const supabase = useSupabaseClient()
+
+  const [imageUrl, setImageUrl] = useState<string | null>(null)
+
+  useEffect(() => {
+    if (url) downloadImage(url)
+  }, [url])
+
+  const downloadImage = async (path: string) => {
+    try {
+      const { data, error } = await supabase.storage.from('images').download(path)
+      if (error) {
+        throw error
+      }
+      const url = URL.createObjectURL(data)
+      setImageUrl(url)
+    } catch (error) {
+      console.log('Error downloading image: ', error)
+    }
+  }
+}
+
+export default Image
