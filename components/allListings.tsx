1c31ea2a81a2fb3fceed24f679f101386961685c
evazuka <eva.zhuk@gmail.com>
2022-11-05 14:24:03 +0200

    added grid

--- components/allListings.tsx
+++ components/allListings.tsx
@@ -1,6 +1,7 @@
-import { Box, Heading} from "@chakra-ui/react"
-import { useSupabaseClient} from "@supabase/auth-helpers-react"
+import { Box, Button, Grid, GridItem, Heading } from "@chakra-ui/react"
+import { useSupabaseClient } from "@supabase/auth-helpers-react"
 import { useEffect, useState } from "react"
+import { Image } from './image'
 
 export const AllListings = () => {
     const supabase = useSupabaseClient()
@@ -25,18 +26,45 @@ export const AllListings = () => {
     }
 
     return <>
-    <Box>
-      <Heading my='2'>What you can take:</Heading>
-      {listings.map(({ name, description }, key) => <Listing key={key} name={name} description={description} />)}
-    </Box>
-  </>
+        <Box>
+            <Heading my='2'>What you can take:</Heading>
+            {listings.map(({ name, description, imageUrl }, key) => <Listing key={key} name={name} description={description} imageUrl={imageUrl} />)}
+        </Box>
+    </>
 
 }
 
-export const Listing = ({ name, description }: { name: string, description: string }) => {
+export const Listing = ({ name, description, imageUrl }: { name: string, description: string, imageUrl: string | undefined }) => {
     return <Box my='4' px='16' py='4' borderWidth='1px' borderRadius='lg'>
-      {name} - {description}
+        <Grid
+            templateAreas={`"header image"
+                  "description image"
+                  "button image"`}
+            gridTemplateRows='repeat(3, 1fr)'
+            gridTemplateColumns='repeat(2, 1fr)'
+            h='100px'
+        >
+            <GridItem area={'header'}>
+                <Heading size='md'>{name}</Heading>
+            </GridItem>
+            <GridItem area={'description'}>
+                {description}
+            </GridItem>
+            <GridItem area={'image'}>
+                {imageUrl
+                    ? <Image url={imageUrl} />
+                    : <img
+                        src='https://via.placeholder.com/100?text=?'
+                        alt="image"
+                        className="image"
+                        style={{ height: 100, width: 100 }}
+                    />}
+            </GridItem>
+            <GridItem area={'button'}>
+                <Button>want this!</Button>
+            </GridItem>
+        </Grid>
     </Box>
-  }
+}
 
 export default AllListings
